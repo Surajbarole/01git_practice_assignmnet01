db5fbd660a61d1e678001ee84d02fb68af5f28cf
Suraj barole <you@example.com>
2022-10-19 11:52:29 +0530

    Updating

---
+++ Palindrome.js
@@ -0,0 +1,11 @@
+let str="madam"
+let bag=""
+for(let i=str.length-1;i>=10;i--){  
+   bag=sum+str[i]
+  }
+
+if(bag==str){
+  console.log("Palindrome")
+  }else {
+   console.log("Not Palindrome")
+   }
---
+++ Prime.js
@@ -0,0 +1,12 @@
+let num=13
+let count=0
+for(let i=0;i<=num;i++){
+if(i%num==0){
+count--   
+    }
+  }
+  if(count==0){
+   console.log("true")
+    }else{
+   console.log("false")
+  }
